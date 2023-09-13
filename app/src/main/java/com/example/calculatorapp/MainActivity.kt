feadb7dd90ef75985e13180b3f72e752cf9cc66a
Vivek kumar <vkvivek2000@gmail.com>
2023-09-13 12:19:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,208 @@
+package com.example.calculatorapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.calculatorapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var data: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.apply {
+
+
+            btn0.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "0"
+
+
+            }
+
+            btn1.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "1"
+
+
+            }
+
+            btn2.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "2"
+
+
+            }
+
+            btn3.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "3"
+
+
+            }
+
+            btn4.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "4"
+
+
+            }
+
+            btn5.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "5"
+
+
+            }
+
+            btn6.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "6"
+
+
+            }
+
+            btn7.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "7"
+
+
+            }
+
+            btn8.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "8"
+
+
+            }
+
+            btn9.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = data + "9"
+
+
+            }
+
+            btnAc.setOnClickListener {
+
+                inputTxt.text = ""
+                outputTxt.text = ""
+
+            }
+
+            btnDot.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data."
+
+
+            }
+
+            btnPlus.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data+"
+
+            }
+
+            btnMinus.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data-"
+
+            }
+
+            btnModule.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data%"
+
+            }
+
+            btnMultiplication.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data×"
+
+            }
+            btnDivision.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data÷"
+
+
+            }
+
+            binding.btnPi.setOnClickListener {
+
+                data = inputTxt.text.toString()
+                inputTxt.text = "$data"+"π"
+
+
+            }
+
+
+            btnEqual.setOnClickListener {
+
+                if (inputTxt.text == "") {
+
+                } else {
+
+                    try {
+                        data = inputTxt.text.toString()
+
+                        data = data!!.replace("×".toRegex(), "*")
+                        data = data!!.replace("%".toRegex(), "/100")
+                        data = data!!.replace("÷".toRegex(), "/")
+                        data = data!!.replace("π".toRegex(), "3.14")
+
+                        val rhino: Context = Context.enter()
+                        rhino.optimizationLevel = -1
+
+                        var finalResult = ""
+
+                        val scriptable: Scriptable = rhino.initStandardObjects()
+                        finalResult =
+                            rhino.evaluateString(scriptable, data, "Javascript", 1, null).toString()
+
+                        outputTxt.text = finalResult
+
+                    } catch (e: Exception) {
+
+                        Snackbar.make(it, "Wrong Input", Snackbar.LENGTH_LONG).show()
+
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
